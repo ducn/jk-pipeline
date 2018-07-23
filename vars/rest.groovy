e4d33a0f4c465ee230ca972f152a468671b4c63b
labs01 <ngoduc.smpt@gmail.com>
2018-07-23 22:40:01 +0700

    a

--- vars/rest.groovy
+++ vars/rest.groovy
@@ -2,5 +2,6 @@ import is.cinnamon.JenkinsHttpClient
 
 def print_ghibli_films() {
     JenkinsHttpClient http = new JenkinsHttpClient()
-    println(http.get('https://ghibliapi.herokuapp.com/films/'))
+    dataset = readJSON text:http.get('http://176.32.82.108:5000/api/datasets/fetch?ids=t9iy')
+    print(dataset)
 }
---
+++ vars/test.groovy
@@ -0,0 +1,5 @@
+def call(Map parameters) {
+  node {
+    rest.print_ghibli_films()
+  }
+}
