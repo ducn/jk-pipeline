bd042d9a3fb53c6ba41c7f1cd10f09c024711848
labs01 <ngoduc.smpt@gmail.com>
2018-07-23 23:31:05 +0700

    a

--- vars/evaluateModelPipeline.groovy
+++ vars/evaluateModelPipeline.groovy
@@ -2,9 +2,9 @@ def call(Map parameters) {
   node {
     def API_DOMAIN = 'http://176.32.82.108:5000'
     stage ('Download datasets') {
-      def datasets = httpRequest "$API_DOMAIN/api/datasets/fetch?ids=t9iy"
-      print(datasets)
-      datasets = readJSON text:datasets
+      def response = httpRequest "$API_DOMAIN/api/datasets/fetch?ids=t9iy"
+      print(response.content)
+      datasets = readJSON text:response.content
       print(datasets)
       for (dataset in datasets) {
             print("$dataset.package_path")
@@ -18,7 +18,7 @@ def call(Map parameters) {
           version:"1.0.0",
           accuracy:"1",
       ]
-      response = httpRequest consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: body, url: "https://${API_DOMAIN}/api/models/save_result", validResponseCodes: '200'
+      response = httpRequest consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: body, url: "https://$API_DOMAIN/api/models/save_result", validResponseCodes: '200'
       print(response)
     }
   }
