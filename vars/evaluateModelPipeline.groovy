7e2b8ff4c55b6f6e1f33f985569f6fa09768f6af
labs01 <ngoduc.smpt@gmail.com>
2018-07-23 23:25:50 +0700

    a

--- vars/evaluateModelPipeline.groovy
+++ vars/evaluateModelPipeline.groovy
@@ -2,10 +2,13 @@ def call(Map parameters) {
   node {
     def API_DOMAIN = 'http://176.32.82.108:5000'
     stage ('Download datasets') {
-      def datasets = httpRequest "{$API_DOMAIN}/api/datasets/fetch?ids=t9iy"
+      def datasets = httpRequest "$API_DOMAIN/api/datasets/fetch?ids=t9iy"
+      print(datasets)
+      datasets = readJSON text:datasets
+      print(datasets)
       for (dataset in datasets) {
-            print("{$dataset.package_path}")
-            sh "wget {$API_DOMAIN}/{$dataset.package_path} -P /data/model/"
+            print("$dataset.package_path")
+            sh "wget $API_DOMAIN/$dataset.package_path -P /data/model/"
       }
     }
     stage ('Saving accuracy score') {
